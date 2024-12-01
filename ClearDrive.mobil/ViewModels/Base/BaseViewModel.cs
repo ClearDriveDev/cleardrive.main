131cc1f17a19b949d1b78f0649f9befc80b5a7a6
b0kovada <b0kovada@vasvari.org>
2024-12-01 17:59:53 +0100

    mobil redo

--- ClearDrive.mobil/ViewModels/Base/BaseViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace ClearDrive.mobil.ViewModels.Base
-{
-    public abstract class BaseViewModel : ObservableObject
-    {
-    }
-}
--- ClearDrive.mobil/ViewModels/Base/IAsyncInitialization.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace ClearDrive.mobil.ViewModels.Base
-{
-    public interface IAsyncInitialization
-    {
-        public Task InitializeAsync();
-    }
-}
