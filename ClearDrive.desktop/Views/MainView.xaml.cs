3e0dd805d46dfc79ea1b922bd2957bd4ce0552e0
b0kovada <b0kovada@vasvari.org>
2024-12-01 19:03:50 +0100

    kokany, de jo

--- ClearDrive.desktop/Views/MainView.xaml.cs
+++ ClearDrive.desktop/Views/MainView.xaml.cs
@@ -1,13 +1,35 @@
 ﻿using System.Windows;
+using ClearDrive.desktop.Views;
+using ClearDrive.desktop.Views.Content;
 
 namespace ClearDrive.desktop.Views
 {
+
     public partial class MainView : Window
     {
+        public object CurrentView
+        {
+            get { return (object)GetValue(CurrentViewProperty); }
+            set { SetValue(CurrentViewProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrentViewProperty =
+            DependencyProperty.Register("CurrentView", typeof(object), typeof(MainView), new PropertyMetadata(null));
+
         public MainView()
         {
             InitializeComponent();
+            DataContext = this;
+        }
 
+        private void OnTodoButtonClick(object sender, RoutedEventArgs e)
+        {
+            CurrentView = new DragAndDropTablePage(); 
+        }
+
+        private void OnMapButtonClick(object sender, RoutedEventArgs e)
+        {
+            CurrentView = new MapPage(); 
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
